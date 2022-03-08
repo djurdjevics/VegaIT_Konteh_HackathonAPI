9856a6a5d15c9ee456572929a34a80292765bbbf
Stefan Djurdjevic <djurdjevics@outlook.com>
2022-03-08 13:40:50 +0100

    Repository layer added

---
+++ VegaIT_Konteh_Hakaton.Repository/FacultyRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VegaIT_Konteh_HakatonAPI.Data.Context;
+using VegaIT_Konteh_HakatonAPI.Data.Entities;
+
+namespace VegaIT_Konteh_Hakaton.Repository
+{
+    public interface IFacultyRepository:IRepository<Faculty>
+    {
+    }
+    public class FacultyRepository : IFacultyRepository
+    {
+        private HackathonContext _hackathonContext;
+        public FacultyRepository(HackathonContext hackathonContext)
+        {
+            _hackathonContext = hackathonContext;
+        }
+        public async Task<IEnumerable<Faculty>> GetAllAsync()
+        {
+            List<Faculty> result = await _hackathonContext.Faculties.Include(x => x.WorkingHours)
+                .Include(x => x.Rooms)
+                .ThenInclude(x=>x.Desks)
+                .ToListAsync();
+
+            return result;
+        }
+    }
+}
---
+++ VegaIT_Konteh_Hakaton.Repository/RoomRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VegaIT_Konteh_HakatonAPI.Data.Context;
+using VegaIT_Konteh_HakatonAPI.Data.Entities;
+
+namespace VegaIT_Konteh_Hakaton.Repository
+{
+    public interface IRoomRepository:IRepository<Room>
+    {
+        Task<IEnumerable<Room>> GetByIDAsync(Guid id);
+    }
+    public class RoomRepository : IRoomRepository
+    {
+        private HackathonContext _hackathonContext;
+        public RoomRepository(HackathonContext hackathonContext)
+        {
+            _hackathonContext = hackathonContext;
+        }
+
+
+        public async Task<IEnumerable<Room>> GetAllAsync()
+        {
+            return await _hackathonContext.Rooms.Include(x=>x.Faculty).Include(x=>x.Desks).ToListAsync(); 
+        }
+
+        public async Task<IEnumerable<Room>> GetByIDAsync(Guid id)
+        {
+            return await _hackathonContext.Rooms.Include(x => x.Faculty).Include(x => x.Desks).Where(x => x.FacultyID == id).ToListAsync();
+        }
+    }
+}

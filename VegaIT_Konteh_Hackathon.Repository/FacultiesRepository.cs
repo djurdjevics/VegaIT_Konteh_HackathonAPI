953b65448b35a30f25bb01b9716f421c747ab6d2
Stefan Djurdjevic <djurdjevics@outlook.com>
2022-03-08 02:19:46 +0100

    Added Data Layer

---
+++ VegaIT_Konteh_Hackathon.Repository/FacultiesRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VegaIT_Konteh_HakatonAPI.Data.Context;
+using VegaIT_Konteh_HakatonAPI.Data.Entities;
+
+namespace VegaIT_Konteh_Hackathon.Repository
+{
+    public interface IFacultiesRepository : IRepository<Faculty>
+    {
+
+    }
+
+    public class FacultiesRepository:IFacultiesRepository
+    {
+        private HackathonContext _hackathonContext;
+
+        public FacultiesRepository(HackathonContext hackathonContext)
+        {
+            _hackathonContext = hackathonContext;
+        }
+        public async Task<IEnumerable<Faculty>> GetAll()
+        {
+            List<Faculty> result = await _hackathonContext.Faculties.ToListAsync();
+            return result;
+        }
+    }
+}
---
+++ VegaIT_Konteh_HakatonAPI.Data/Entities/Faculty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VegaIT_Konteh_HakatonAPI.Data.Entities
+{
+    [Table("Faculty")]
+    public class Faculty
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        [Column("WorkingHours")]
+        public int WorkingHour { get; set; }
+        public virtual WorkingHours WorkingHours { get; set; }
+        public virtual ICollection<Room> Rooms { get; set; }
+    }
+}
